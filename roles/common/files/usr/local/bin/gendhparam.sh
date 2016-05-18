cda53ea254de51eb46cb0f53f7d33b9a0f794bfc
Guilhem Moulin <guilhem@fripost.org>
2016-05-18 17:55:40 +0200

    Move /etc/ssl/private/dhparams.pem to /etc/ssl/dhparams.pem and make it public.
    Ideally we we should also increase the Diffie-Hellman group size from
    2048-bit to 3072-bit, as per ENISA 2014 report.
    
        https://www.enisa.europa.eu/publications/algorithms-key-size-and-parameters-report-2014
    
    But we postpone that for now until we are reasonably certain that older
    client won't be left out.

--- roles/common/files/usr/local/bin/gendhparam.sh
+++ roles/common/files/usr/local/bin/gendhparam.sh
@@ -3,9 +3,8 @@
 set -ue
 PATH=/usr/bin:/bin
 
-privkey="$1"
+out="$1"
 bits="${2:-2048}"
-rand=
 
-install --mode=0600 /dev/null "$privkey"
-openssl dhparam -rand "${rand:-/dev/urandom}" "$bits" >"$privkey"
+install --mode=0644 /dev/null "$out"
+openssl dhparam "$bits" >"$out"
