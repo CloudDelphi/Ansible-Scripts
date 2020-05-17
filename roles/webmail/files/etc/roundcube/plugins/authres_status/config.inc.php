aba1438a4d188ae4ba20d2ee264212622809afd1
Guilhem Moulin <guilhem@fripost.org>
2020-05-17 04:25:36 +0200

    Roundcube: Port to Debian 10.
    We use the version from buster-backports (currently 1.4.4+dfsg.1-1~bpo10+1)
    for the elastic theme.

---
+++ roles/webmail/files/etc/roundcube/plugins/authres_status/config.inc.php
@@ -0,0 +1,5 @@
+<?php
+
+$config['trusted_mtas'] = array('mx1.fripost.org', 'mx2.fripost.org');
+
+?>
---
+++ roles/webmail/files/etc/roundcube/plugins/html5_notifier/config.inc.php
@@ -0,0 +1,6 @@
+<?php
+
+$config['html5_notifier_duration'] = '3';
+$config['html5_notifier_smbox'] = '1';
+
+?>
